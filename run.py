d654df3d3a2d4e6536cbdceffcb71f7bf9581816
Dan <daniel.leonssio@hotmail.com>
2024-08-07 22:34:04 -0300

    Config: Criando conexões com SQLAlchemy

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from src import UserRepo
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello_world():
     return 'Olá, estou na aplicação setada'
+
+@app.route("/insert", methods=["POST"])
+def insert():
+
+    userRepo = UserRepo()
+    body = request.json
+
+    userRepo.insert_user(body["name"])
+
+    return "OK"
